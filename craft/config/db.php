2ffc88130fac6ac45571468f3fd2d1e0756d6da9
Jon Farrell <jon@jonfarrell.io>
2018-04-07 17:14:56 -0400

    First Commit, blank slate

---
+++ craft/config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Database Configuration
+ *
+ * All of your system's database configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ */
+
+return array(
+	'server' => 'localhost',
+	'database' => 'craft2',
+	'user' => 'root',
+	'password' => 'root',
+	'tablePrefix' => 'craft',
+);
